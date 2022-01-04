b3be25bf5b225d9f2c1a81b519015e14f9aab105
Ken Li <krli@ualberta.ca>
2022-01-03 23:36:09 -0700

    Style Changer

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["Software Developer", "Video Game Developer", "Web Developer", "Graphic Designer"],
+    typeSpeed: 50,
+    backSpeed: 75,
+    loop: true
+})
