c64125821d9332644159ad280d85556fd9dac58c
Ken Li <krli@ualberta.ca>
2022-01-04 00:31:51 -0700

    Responsive Sidebar

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ var typed = new Typed(".typing", {
     backSpeed: 75,
     loop: true
 })
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSections = document.querySelectorAll(".section"),
+    totalSections = allSections.length;
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    sidebar = document.querySelector(".sidebar");
+
+navTogglerBtn.addEventListener("click", () => {
+    sidebarSectionTogglerBtn();
+})
+
+function sidebarSectionTogglerBtn()
+{
+    sidebar.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for (let i = 0; i < totalSections; i++)
+    {
+        allSections[i].classList.toggle("open");
+    }
+}
